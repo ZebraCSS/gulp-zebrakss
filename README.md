4c8bc717682330c83eb664918eead2cb05b37fd7
Julia Rechkunova <julia.rechkunova@gmail.com>
2014-09-30 12:15:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# gulp-zebrakss
+
+Gulp plugin for KSS ([Knyle Stype Sheets](http://warpspire.com/kss/)) documentation generation.
+
+This plugin is based on [kss-node](https://github.com/hughsk/kss-node) and generates a styleguide based on code documentation.
+
+**Plugin isn't ready**
---
+++ index.js
@@ -0,0 +1,51 @@
+var through = require('through2'),
+    gutil = require('gulp-util'),
+    ExecBuffer = require('exec-buffer'),
+    kss = require('kss-node').path;
+
+// consts
+const PLUGIN_NAME = 'gulp-zebrakss';
+
+// plugin level function (dealing with files)
+function gulpZebraKSS(options) {
+    options = options || {};
+
+    var exec = new ExecBuffer(),
+        args = [exec.src()];
+
+    if (options.template) {
+        args.push('--template', options.template);
+    }
+
+    if (options.preprocessor) {
+        args.push('--' + options.preprocessor);
+    }
+
+    return through.obj(function (file, enc, cb) {
+        if (file.isNull()) {
+            cb(null, file);
+            return;
+        }
+
+        if (file.isStream()) {
+            cb(new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return;
+        }
+
+        exec.use(kss, args)
+            .run(file.contents, function (err, buf) {
+                if (err) {
+                    cb(new gutil.PluginError(PLUGIN_NAME, err, {
+                        fileName: file.path
+                    }));
+                    return;
+                }
+
+                file.contents = buf;
+                cb(null, file);
+            });
+    });
+}
+
+// exporting the plugin main function
+module.exports = gulpZebraKSS;
